c86834cf6be878c5f4a01730a6d985dcbfa1a90e
CharlZ <charles.ceo2020@gmail.com>
2020-04-30 00:28:40 -0600

    added debugger display session

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="main.css">
-    <title>To-Do PHP list!</title>
-</head>
-
-<body>
-    <form method="POST">
-        <h2> Add a To-Do </2>
-        <input type="text" name="todo_item" placeholder="Enter your item">
-        <button>Add to list</button>
-        <button>Reset</button>
-    <h3> Active To-Do </h3>
-        <p><input type="checkbox" name="item[]" value="jump" />  Jump</p>
-        <p><input type="checkbox" name="item[]" value="run" />  Run</p>
-        <p><input type="checkbox" name="item[]" value="skip" />  Skip</p>
-        <p><input type="submit" name="submit" value="Complete" /></p>
-        </form>
-</body>
-</html>
-
 <?php 
     $item = $_POST["item"];
     echo $items[0];
@@ -41,4 +16,38 @@ if ( isset( $_POST['submit'] ) )
         echo 'Please select an item';
     }
 }
-?>
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do PHP list!</title>
+</head>
+
+<body>
+    <form method="POST">
+        <h2> Add a To-Do </2>
+        <input type="text" name="todo_item" placeholder="Enter your item">
+        <button>Add to list</button>
+        <button>Reset</button>
+    <h3> Active To-Do </h3>
+        <p><input type="checkbox" name="item[]" value="jump" />  Jump</p>
+        <p><input type="checkbox" name="item[]" value="run" />  Run</p>
+        <p><input type="checkbox" name="item[]" value="skip" />  Skip</p>
+        <p><input type="submit" name="submit" value="Complete" /></p>
+        </form>
+
+    <h4> Debugging</h4>
+    <pre>
+    Click to Expnad!
+        <strong>SESSION:</strong>
+        <?php var_dump( $_SESSION ); ?>
+    </pre>
+    <pre>
+        <strong>POST:</strong>
+        <?php var_dump( $_POST ); ?>
+    </pre>
+</body>
+</html>
+
